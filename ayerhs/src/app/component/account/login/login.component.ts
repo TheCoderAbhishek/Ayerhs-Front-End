8b88378468c2e1b49bd9ef18952ebcee2a9fab4e
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-06-22 15:48:19 +0530

    Consume `LoginClient` Endpoint

--- ayerhs/src/app/component/account/login/login.component.ts
+++ ayerhs/src/app/component/account/login/login.component.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -12,5 +15,29 @@ export class LoginComponent {
 
   togglePasswordVisibility(): void {
     this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  }
+
+  inLoginClientDto: InLoginClientDto;
+  constructor(private http: HttpClient) {
+    this.inLoginClientDto = new InLoginClientDto();
+  }
+
+  onLogin() {
+    this.http.post('https://localhost:44302/ayerhs-security/Account/LoginClient', this.inLoginClientDto).subscribe((response:any) => {
+      if (response.response === 1){
+        alert("Login Successful");
+      } else {
+        alert("Login Unuccessful");
+      }
+    })
+  }
+}
+
+export class InLoginClientDto{
+  ClientEmail: string;
+  ClientPassword: string;
+  constructor(){
+    this.ClientEmail='',
+    this.ClientPassword=''
   }
 }
