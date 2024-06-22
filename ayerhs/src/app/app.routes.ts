1936d253f179c9b1902b84b477d792c292af4fd1
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-06-22 14:33:46 +0530

    Basic Routes Added

--- ayerhs/src/app/app.routes.ts
+++ ayerhs/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/account/login/login.component';
+import { RegistrationComponent } from './component/account/registration/registration.component';
+import { LayoutComponent } from './component/layout/layout/layout.component';
+import { DashboardComponent } from './component/layout/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
