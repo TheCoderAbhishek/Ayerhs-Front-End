82d3d02748a1a18042ce87c9e4aa8f82376acbd2
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-06-24 07:29:04 +0530

    Include `Activate User` link on Login Page

--- ayerhs/src/app/app.routes.ts
+++ ayerhs/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RegistrationComponent } from './component/account/registration/registra
 import { LayoutComponent } from './component/layout/layout/layout.component';
 import { DashboardComponent } from './component/layout/dashboard/dashboard.component';
 import { OtpVerificationComponent } from './component/account/otp-verification/otp-verification.component';
+import { ActivateAccountComponent } from './component/account/activate-account/activate-account.component';
 
 export const routes: Routes = [
   {
@@ -22,6 +23,10 @@ export const routes: Routes = [
   {
     path: 'otp-verification',
     component: OtpVerificationComponent,
+  },
+  {
+    path: 'activate-account',
+    component: ActivateAccountComponent
   },
   {
     path: '',
